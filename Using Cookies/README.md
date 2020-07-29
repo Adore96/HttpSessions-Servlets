51676b1610998c8b53d09f6284f4958d7d6bd7b0
Kalhara Perera <adorekasun@gmail.com>
2020-07-29 13:21:47 +0530

    Using Cookies Updated

--- Using Cookies/README.md
+++
@@ -1,2 +0,0 @@
-# HttpSessions-Servlets
- 
--- Using Cookies/src/com/Adore96/SqServlet.java
+++ Using Cookies/src/com/Adore96/SqServlet.java
@@ -1,27 +1,29 @@
 package com.Adore96;
 
-import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpSession;
 import java.io.IOException;
 import java.io.PrintWriter;
 
-@WebServlet(name = "SqServlet")
-public class SqServlet extends HttpServlet {
-    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+public class SqServlet extends javax.servlet.http.HttpServlet {
+    protected void doPost(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
 
     }
 
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    protected void doGet(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
         PrintWriter out = response.getWriter();
 
         System.out.println("Connection Success");
 
-        HttpSession session = request.getSession();
-        out.println((int)session.getAttribute("k"));
+        int k = 0;
+        Cookie cookie[] = request.getCookies();
 
+        for (Cookie c : cookie){
+            if (c.getName().equals("cookieName")){
+                k = Integer.parseInt(c.getValue());
+            }
+        }
+
+        out.println(k);
     }
 }
